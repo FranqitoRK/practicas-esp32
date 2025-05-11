a6dff45aaf3c119172aaac37bfb104ba6b5e620c
franqitork <franco.a.maio@gmail.com>
2025-05-10 23:08:54 -0300

    adding readme to both exercises

---
+++ 09.05/README.md
@@ -0,0 +1,2 @@
+## Producer-Consumer | Second activity
+This code implements another producer-consumer system using FreeRTOS tasks on an ESP32, but this time it controls an RGB LED based on queued commands.
--- 09.05/data.h
+++ 09.05/data.h
@@ -4,13 +4,12 @@
 #include <Arduino.h>
 
 enum LedCommand {
-    LED_RED, // Enciende el LED rojo
-    LED_GREEN, // Enciende el LED verde
-    LED_BLUE, // Enciende el LED azul
-    LED_BLINK // Parpadean los 3 LEDs
+    LED_RED,
+    LED_GREEN,
+    LED_BLUE,
+    LED_BLINK
 };
 
-// Devuelve un comando LED aleatorio
 LedCommand randomCommand();
 
 #endif 
